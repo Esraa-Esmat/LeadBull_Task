99e4bf39d847e3a263030e294657c46672f99168
Esraa-Esmat <esraamahmoudesmat60@gmail.com>
2024-08-12 20:43:49 +0300

    edit for responsive

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -10,7 +10,7 @@ const Profile = () => {
                 <h2 className="profile-title">PROFILE</h2>
                 <Container className='py-4'>
                     <Row className="justify-content-center">
-                        <Col md={4} sm={12} className="my-auto p-5">
+                        <Col md={4} sm={12} className="my-auto p-xl-5 p-md-0">
                             <Col md={12} sm={12} >
                                 <Card className="text-center bg-transparent text-white profile-box">
                                     <Card.Body>
@@ -21,7 +21,7 @@ const Profile = () => {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col md={12} sm={12} className='mt-5'>
+                            <Col md={12} sm={12} className='mt-xl-5 mt-md-2 p-xl-5 p-md-0'>
                                 <Card className="text-center bg-transparent text-white profile-box">
                                     <Card.Body>
                                         <h2 className="profile-box-title fw-bold rounded-5">Values</h2>
@@ -32,7 +32,7 @@ const Profile = () => {
                                 </Card>
                             </Col>
                         </Col>
-                        <Col md={4} sm={12} className=" my-auto p-5">
+                        <Col md={4} sm={12} className=" my-auto  p-xl-5 p-md-2">
                             <Card className="text-center bg-transparent text-white profile-box">
                                 <Card.Body>
                                     <h2 className="profile-box-title fw-bold rounded-5">Mission</h2>
@@ -43,7 +43,7 @@ const Profile = () => {
                             </Card>
                         </Col>
 
-                        <Col md={4} sm={12} className="text-center my-auto p-5">
+                        <Col md={4} sm={12} className="text-center my-auto  p-xl-5 p-md-4">
                             <Image src={logo2} alt="Leadbull LLC" className="profile-image mb-2" fluid />
                             <p className="company-name">LEADBULL LLC</p>
                         </Col>
