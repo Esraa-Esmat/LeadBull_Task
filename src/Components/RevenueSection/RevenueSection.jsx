9d9087f62432cc75688af1e322320495150e1c06
Esraa-Esmat <esraamahmoudesmat60@gmail.com>
2024-08-12 22:37:04 +0300

    edit for responsive

--- src/Components/RevenueSection/RevenueSection.jsx
+++ src/Components/RevenueSection/RevenueSection.jsx
@@ -1,53 +1,47 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import './RevenueSection.css';
 
 const RevenueSection = () => {
     return (
         <Container fluid className="text-center p-5 revenue-section">
-            <Row className="justify-content-center revenue-section-header ">
+            <Row className="justify-content-center revenue-section-header">
                 <Col md={10}>
                     <h2 className="secondSectionTitle mb-4">OVER $9 MILLION</h2>
-
-                    <div style={{ font: 'Roboto Flex', width: '90%' }} className=' m-auto '>
-                        <p className=' text-center display-6 py-2 fw-bold'>In combined revenue generated for our contractor clients</p>
-                        <p className=' text-center display-6'> Our services allow clients to gain a better chance of fulfilling customers' needs and catching their interest,
+                    <div className='revenue-section-headerDiv m-auto'>
+                        <p className='text-center py-2 fw-bold'>In combined revenue generated for our contractor clients</p>
+                        <p className='text-center'>Our services allow clients to gain a better chance of fulfilling customers' needs and catching their interest,
                             leading to higher net revenue and sustained customer satisfaction and loyalty.
                             Many top global contractors <a href="#readmore">Read more.</a></p>
                     </div>
-
                 </Col>
             </Row>
-            <div className='revenue-section-container1  m-auto'>
-                <div className='revenue-section-container px-3 py-4   m-auto mt-5' >
-                    <Row className="mt-4 justify-content-center container">
-                        <Col md={3}>
+            <div className='revenue-section-container1 m-auto'>
+                <div className='revenue-section-container px-3 py-4 m-auto mt-xl-5 mt-md-0'>
+                    <Row className=" justify-content-center w-100 my-auto my-xl-3">
+                        <Col xs={12} sm={6} md={3} >
                             <div>
                                 <h3>75+</h3>
                                 <p>Independent Member Firms</p>
                             </div>
-
                         </Col>
-                        <Col md={3}>
-                            <div className='border-left'>
+                        <Col xs={12} sm={6} md={3} className=" border-div">
+                            <div>
                                 <h3>35+</h3>
                                 <p>Contracting LLCs</p>
                             </div>
-
                         </Col>
-                        <Col md={3}>
-                            <div className='border-left'>
+                        <Col xs={12} sm={6} md={3} className=" border-div">
+                            <div>
                                 <h3>75+</h3>
                                 <p>Affiliate Network</p>
                             </div>
-
                         </Col>
-                        <Col md={3}>
-                            <div className='border-left'>
+                        <Col xs={12} sm={6} md={3} className="border-div">
+                            <div>
                                 <h3>75+</h3>
                                 <p>Countries</p>
                             </div>
-
                         </Col>
                     </Row>
                 </div>
